761c7dbed6e6d9a4d8a8fad495174983f3a36814
BrennerG <gabrielbrbr@gmail.com>
2020-01-12 18:35:59 +0100

    ADD: feature descriptions & streamlit notebook

--- data_prep.py
+++ data_prep.py
@@ -1,18 +1,69 @@
 import pandas as pd 
+import streamlit as st
 
-# read source csv
-raw = pd.read_csv("unesco_poverty_dataset.csv") 
-keys = raw.DEMO_IND.unique() 
+st.markdown("# Data Preparation - DOPP3")
+st.markdown('_creating a data set to predict extreme poverty_')
+st.markdown("## Transform base data")
+with st.echo():
+    # READ SOURCE CSV
+    raw = pd.read_csv("unesco_poverty_dataset.csv") 
+    keys = raw.DEMO_IND.unique() 
 
-# define base csv
-base = raw[['LOCATION', 'TIME']]
+    # DEFINE BASE CSV
+    base = raw[['LOCATION', 'TIME']]
 
-# for every var join on LOCATION & TIME 
-for i in range(0,len(keys)):
-    loop = raw.loc[raw.DEMO_IND == keys[i]]
-    base = pd.merge(base, loop[['LOCATION', 'TIME', 'Value']],  how='left', left_on=['LOCATION','TIME'], right_on = ['LOCATION','TIME']) 
-    base.columns = base.columns.str.replace('Value', keys[i])
+    # FOR EVERY VAR JOIN ON LOCATION & TIME 
+    for i in range(0,len(keys)):
+        loop = raw.loc[raw.DEMO_IND == keys[i]]
+        base = pd.merge(base, loop[['LOCATION', 'TIME', 'Value']],  how='left', left_on=['LOCATION','TIME'], right_on = ['LOCATION','TIME']) 
+        base.columns = base.columns.str.replace('Value', keys[i])
 
-# export
-base.to_csv('transformed.csv', sep=',', na_rep="NA")
-exit
+    # DROP DUPLICATES
+    base = base.drop_duplicates()
+    st.write(base.head(100))
+
+st.markdown("## Features")
+with st.echo():
+    # GET NA DATA PER COLUMN
+    na_percent = []
+    na_total = []
+    minimum = []
+    maximum = []
+    for col in base.columns:
+        na_percent.append(round(base[col].isna().sum() / base.shape[0] * 100, 2))
+        na_total.append(base[col].isna().sum())
+        minimum.append(base[col].min())
+        maximum.append(base[col].max())
+
+    # GET VARIABLE DESCRIPTIONS
+    descriptions = raw['Indicator'].drop_duplicates().tolist()
+    descriptions.insert(0, 'LOCATION')
+    descriptions.insert(1, 'TIME')
+
+    features = pd.DataFrame(
+        {'descriptions': descriptions, 
+        'na_percent': na_percent, 
+        'na_total': na_total,
+        'minimum': minimum,
+        'maximum': maximum},
+        index=base.columns) 
+    st.write(features)
+    
+st.markdown("## Target Column")
+st.markdown('_what is the target column supposed to be?_')
+with st.echo():
+    # calculate GNI per day (PPP)
+    base['target'] = base['NY_GNP_PCAP_PP_CD'] / 365
+    poor = base[base.target < 2][['LOCATION', 'TIME', 'target']]
+
+st.write('Poor countries EVER in %', poor['LOCATION'].drop_duplicates().shape[0] / base['LOCATION'].drop_duplicates().shape[0] * 100)
+st.write('this cant be right...')
+
+
+# EXPORT BASE DATAFRAME
+base.drop(['target'], axis=1).to_csv('transformed.csv', sep=',', na_rep="NA")
+# EXPORT FEATURE DESCRIPTORS
+features.to_csv('feature_descriptions.csv', sep=',', na_rep="NA")
+
+# BALLOOOOOOONS
+st.balloons()
