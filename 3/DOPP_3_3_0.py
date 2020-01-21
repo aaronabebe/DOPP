95a2fcb2fb79ef9a79867d5248757a2348ee72c2
Carlos Vargas <carlosvargas9103@gmail.com>
2020-01-21 18:43:07 +0100

    folder for question3

--- 3/DOPP_3_3_0.py
+++ 3/DOPP_3_3_0.py
@@ -4,18 +4,32 @@ import streamlit as st
 
 st.markdown("# DOPP - Exercise 3 - Question 3")
 st.markdown("## Importing the dataset from data_prep.py")
-st.markdown('_creating a data set to predict extreme poverty_')
-st.markdown("## Transform base data")
+st.markdown('_The data set looks like')
 with st.echo():
     # READ FROM DATA_PREP
-    
     df_p = pd.read_csv("data/transformed.csv") 
     poor = df_p
-
-
+    
 st.write(poor)
 st.write(poor.shape)
 
+
+st.markdown("## Identify countries in poverty at least once")
+with st.echo():
+    #print (poor.columns)
+    #countries poor.poverty
+
+    p_poor = poor[poor['poverty'] == 1]
+    p_countries = p_poor.LOCATION.unique()
+    p_countries
+
+
+st.write(poor.columns)
+st.write(poor.poverty)
+
+
+
+
 st.markdown("## Target Column")
 
-
+st.balloons()
